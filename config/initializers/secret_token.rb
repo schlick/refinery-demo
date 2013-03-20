4c6cedaefc174da8479927676715c9349ebe4edf
Michael MacDonald <michael@starclass.com.au>
2013-03-20 23:38:19 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinery::Application.config.secret_token = 'e9e60afaa8892987541b21d1de99769367861d08f20acc0a3bca9b46cb5b5180d6a1f8b8aefaca68d98bd302c13e8e2c5fca1cf14e690dc5dc6a767004453289'
